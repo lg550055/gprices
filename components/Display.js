7f7761888b6b7430af161602362b932e9819fd9b
lg550055 <lg550055@hotmail.com>
2022-08-11 22:47:33 -0500

    Adds item details

--- components/Display.js
+++ components/Display.js
@@ -2,8 +2,9 @@ export default function Display({ items }) {
   return (
     <>
       {items.map(item =>
-        <div>
-          <p>{item.name}</p>
+        <div key={item.id}>
+          <p>{item.category} - {item.brand} {item.name}</p>
+          <p>{item.price}, {item.size} {item.unit} - ${item.ppunit} per {item.unit}</p>
         </div>
       )}
     </>
