c3acaffe608f0453da25f01fdc432b0c7a82b8de
lg550055 <lg550055@hotmail.com>
2022-08-12 08:59:52 -0500

    Adds handlesubmit reqobj

--- components/AddForm.js
+++ components/AddForm.js
@@ -1,6 +1,21 @@
 export default function AddForm(){
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const reqObj = {
+      name: e.target.name.value,
+      brand: e.target.brand.value,
+      category: e.target.category.value,
+      size: e.target.size.value *1,
+      unit : e.target.unit.value,
+      price: e.target.price.value *1,
+      ppunit: e.target.price.value/e.target.size.value
+    }
+    console.log(reqObj)
+  }
+
   return (
-    <form method="PUT">
+    <form method="PUT" onSubmit={handleSubmit}>
       <input placeholder="name" name="name" required /><br></br>
       <input placeholder="brand" name="brand" required /><br></br>
       <input placeholder="category" name="category" required /><br></br>
